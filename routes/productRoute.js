ec0351bd91179b1cce389ca6799a56132e8e6480
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-01-23 19:55:25 +0530

    logical blocks on crud operations

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Product = require("../models/product.js");
 
-router.get(`/product`, async (req, res) => {
+router.get("/product", async (req, res) => {
   const getAllProducts = await Product.find();
   if (getAllProducts) {
     res.status(200).json({
@@ -16,26 +16,53 @@ router.get(`/product`, async (req, res) => {
   }
 });
 
-router.post(`/create-product`, async (req, res) => {
-  const newProduct = req.body;
-  const createProduct = new Product({
-    name: newProduct.name,
-    image: newProduct.image,
-    stock: newProduct.stock,
-    category: newProduct.category,
-    price: newProduct.price,
-  });
-  const saveProduct = await createProduct.save();
-
-  if (saveProduct) {
-    res.status(201).json({
-      success: true,
-      result: saveProduct,
-    });
-  } else {
+router.get("/product/:id", async (req, res) => {
+  try {
+    const getOneProduct = await Product.findById(req.params.id);
+    if (getOneProduct) {
+      res.status(200).json({
+        success: true,
+        result: getOneProduct,
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: "Product cannot get",
+      });
+    }
+  } catch (err) {
     res.status(500).json({
-      success: false,
-      message: "product data cannot be created",
+      message: "failed to find product",
+    });
+  }
+});
+
+router.post("/create-product", async (req, res) => {
+  try {
+    const newProduct = req.body;
+    const createProduct = new Product({
+      name: newProduct.name,
+      image: newProduct.image,
+      stock: newProduct.stock,
+      category: newProduct.category,
+      price: newProduct.price,
+    });
+    const saveProduct = await createProduct.save();
+
+    if (saveProduct) {
+      res.status(201).json({
+        success: true,
+        result: saveProduct,
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: "product data cannot be created",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Product creation postponed",
     });
   }
 });
