0d8334ad4f464d8343579709c6845e5410f6bb36
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-02-09 20:36:08 +0530

    hbs setup into express js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const productRouter = require("../routes/productRoute.js");
 const userRouter = require("../routes/userRoute.js");
 const path = require("path");
+const hbs = require("hbs");
 require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 const cors = require("cors");
 app.use(cors());
@@ -19,6 +20,19 @@ app.use(`${apiURL}`, productRouter);
 app.use(`${apiURL2}`, userRouter);
 
 app.use("/images", express.static("./images"));
+
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+
+app.use(express.static(static_path));
+
+app.set("view engine", "hbs");
+app.set("views", template_path);
+
+// client routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 // db connection
 mongoose
