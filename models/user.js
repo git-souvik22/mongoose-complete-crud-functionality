6a24e5275f5e35b6224ec13281bdc1bb0498022a
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-02-13 13:19:26 +0530

    seller register form

--- models/user.js
+++ models/user.js
@@ -48,6 +48,18 @@ const userSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  firmName: {
+    type: String,
+  },
+  proprietor: {
+    type: String,
+  },
+  businessCat: {
+    type: String,
+  },
+  gst: {
+    type: String,
+  },
   logState: {
     type: String,
     default: "out",
