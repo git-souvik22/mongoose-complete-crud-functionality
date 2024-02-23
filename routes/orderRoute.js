e0a8c7e13af4a819ff9a90e558989ab043c00fc6
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-02-23 15:16:17 +0530

    razorpay payment gateway setup

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,38 +1,61 @@
 const router = require("express").Router();
 const { requireLogin } = require("../middlewares/userAuth.js");
 const Order = require("../models/order.js");
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
 
-router.post("/place-order", requireLogin, async (req, res) => {
-  try {
-    const { oid, size, quantity, cid, pay } = req.body;
+const KEY_ID = process.env.RAZORPAY_KEY_ID;
+const KEY_SECRET = process.env.RAZORPAY_SECRET_KEY;
 
-    const createOrder = new Order({
-      oid: oid,
-      size: size,
-      quantity: quantity,
-      cid: cid,
-      pay: pay,
-    });
-    const saveOrder = await createOrder.save();
-    if (saveOrder) {
-      res.status(201).send({
-        success: true,
-        order: saveOrder,
+let instance = new Razorpay({
+  key_id: KEY_ID,
+  key_secret: KEY_SECRET,
+});
+
+router.post("/create-order", requireLogin, async (req, res) => {
+  let { amount } = req.body;
+  var options = {
+    amount: amount * 100,
+    currency: "INR",
+    receipt: "order_rcptid_11",
+  };
+  instance.orders.create(options, function (err, order) {
+    if (err) {
+      res.status(500).send({
+        status: false,
+        message: "unable to create a order",
       });
     } else {
-      res.status(401).send({
-        success: false,
-        message: "Order not saved",
+      res.send({
+        status: true,
+        order,
       });
     }
-  } catch (err) {
+  });
+});
+router.post("/verify-payment", async (req, res) => {
+  let { payment_id, order_id, signature } = req.body;
+
+  let payDetails = order_id + "|" + payment_id;
+  let generated_signature = crypto
+    .createHmac("sha256", KEY_SECRET)
+    .update(payDetails.toString())
+    .digest("hex");
+
+  console.log("gen_signature", generated_signature);
+  console.log("cli_signature", signature);
+
+  if (generated_signature == signature) {
+    res.send({
+      status: true,
+      message: "Payment Done",
+    });
+  } else {
     res.status(500).send({
-      success: false,
-      message: "Something went wrong",
+      status: false,
+      message: "Payment Fail",
     });
   }
 });
-
-// router.post("/start-payment", )
 
 module.exports = router;
